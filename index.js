2e80a56c5faa8cab0be493783c44972b550332c2
Argy <itsargy@gmail.com>
2021-12-13 23:40:54 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const { Client, Intents } = require("discord.js");
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+const { token } = require('./config.json');
+
+client.on("ready", () => {
+  console.log("Logado");
+});
+
+client.on("messageCreate", (message) => {
+    if(message.content == "oi"){
+        console.log("ELE FALO OI")
+    }
+})
+
+client.login(token);
